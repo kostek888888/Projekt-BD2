f63479baf43c8f4adcfd47e558d6b4fc79706326
wojtek <wojtek.klusek2@gmail.com>
2017-04-21 17:22:37 +0200

    Schemat bazy poprawiony
    schemat i create

--- Create.sql
+++ Create.sql
@@ -1,51 +1,45 @@
 DROP TABLE Sprzedaz CASCADE CONSTRAINTS;
+DROP TABLE Paragon CASCADE CONSTRAINTS;
+DROP TABLE Platnosci CASCADE CONSTRAINTS;
 DROP TABLE Pismo CASCADE CONSTRAINTS;
 DROP TABLE Tematyka CASCADE CONSTRAINTS;
-DROP TABLE Dodatek CASCADE CONSTRAINTS;
 DROP TABLE Salon CASCADE CONSTRAINTS;
 DROP TABLE Miasto CASCADE CONSTRAINTS;
-DROP TABLE Czas CASCADE CONSTRAINTS;
+DROP TABLE Dzien CASCADE CONSTRAINTS;
+DROP TABLE Miesiac CASCADE CONSTRAINTS;
+DROP TABLE Rok CASCADE CONSTRAINTS;
 DROP TABLE Sprzedawca CASCADE CONSTRAINTS;
-DROP TABLE Kierownik CASCADE CONSTRAINTS;
-DROP TABLE Osoby CASCADE CONSTRAINTS;
 
-CREATE TABLE Osoby (
-id_osoby NUMBER CONSTRAINT osoby_pk PRIMARY KEY,
-imie VARCHAR2(25) NOT NULL,
-nazwisko VARCHAR(50) NOT NULL,
-data_ur DATE NOT NULL,
-pesel VARCHAR2(11) NULL CONSTRAINT os_pesel_uni UNIQUE,
-adres VARCHAR2(60) NOT NULL,
-telefon VARCHAR2(20) NOT NULL
-);
-
-CREATE TABLE Kierownik (
-id_kierownika NUMBER CONSTRAINT kierownik_pk PRIMARY KEY,
-id_osoby NUMBER NOT NULL,
-data_zatrudnienia DATE NOT NULL,
-data_zwolnienia DATE,
-pensja NUMBER(6) NOT NULL,
-
-CONSTRAINT kier_os_fk FOREIGN KEY(id_osoby) REFERENCES Osoby(id_osoby)
-);
 
 CREATE TABLE Sprzedawca (
 id_sprzedawcy NUMBER CONSTRAINT Sprzedawca PRIMARY KEY,
-id_osoby NUMBER NOT NULL,
-id_kierownika NUMBER NOT NULL,
 data_zatrudnienia DATE NOT NULL,
 data_zwolnienia DATE,
 pensja NUMBER(6) NOT NULL,
-ocena NUMBER(1),                   ---------od 1 do 5
-
-CONSTRAINT sprzed_os_fk FOREIGN KEY(id_osoby) REFERENCES osoby(id_osoby),
-CONSTRAINT sprzed_kier_fk FOREIGN KEY(id_kierownika) REFERENCES Kierownik(id_kierownika)
 );
 
-CREATE TABLE Czas (
-id_czasu NUMBER NOT NULL CONSTRAINT czas_pk PRIMARY KEY,
-data DATE NOT NULL,
-kwartal NUMBER(1) NOT NULL
+CREATE TABLE Rok (
+id_roku NUMBER NOT NULL CONSTRAINT rok_pk PRIMARY KEY,
+rok NUMBER NOT NULL
+);
+
+CREATE TABLE Miesiac (
+id_miesiaca NUMBER NOT NULL CONSTRAINT miesiac_pk PRIMARY KEY,
+id_roku NUMBER NOT NULL,
+miesiac NUMBER NOT NULL,
+
+CONSTRAINT dzien_rok_fk FOREIGN KEY(id_roku) REFERENCES rok(id_roku),
+);
+
+CREATE TABLE Dzien (
+id_dnia NUMBER NOT NULL CONSTRAINT dzien_pk PRIMARY KEY,
+id miesiaca NUMBER NOT NULL,
+id_roku NUMBER NOT NULL,
+dzien NUMBER,
+
+CONSTRAINT dzien_miesiac_fk FOREIGN KEY(id_miesiaca) REFERENCES miesiac(id_miesiaca),
+CONSTRAINT dzien_rok_fk FOREIGN KEY(id_roku) REFERENCES miesiac(id_roku)
+
 );
 
 CREATE TABLE Miasto (
@@ -67,11 +61,6 @@ CONSTRAINT salon_miasto_fk FOREIGN KEY(id_miasta) REFERENCES miasto(id_miasta)
 );
 
 
-CREATE TABLE Dodatek (
-id_dodatku NUMBER NOT NULL CONSTRAINT dodatek_pk PRIMARY KEY,
-nazwa VARCHAR2(50) NOT NULL,
-naklad NUMBER(7) NOT NULL
-);
 
 CREATE TABLE Tematyka (
 id_tematyki NUMBER NOT NULL CONSTRAINT tematyka_pk PRIMARY KEY,
@@ -92,113 +81,17 @@ CONSTRAINT pismo_dodatek_fk FOREIGN KEY(id_dodatku) REFERENCES dodatek(id_dodatk
 CONSTRAINT pismo_tematyka_fk FOREIGN KEY(id_tematyki) REFERENCES tematyka(id_tematyki)
 );
 
-CREATE TABLE Sprzedaz (
-id_sprzedazy NUMBER NOT NULL CONSTRAINT sprzedarz_pk PRIMARY KEY,
-id_pisma NUMBER NOT NULL,
-id_salonu NUMBER NOT NULL,
-id_czasu NUMBER NOT NULL,
-id_sprzedawcy NUMBER NOT NULL,
-ilosc_sprzedanych NUMBER NOT NULL,
-suma_zysku NUMBER NOT NULL,
-
-CONSTRAINT sprzedarz_pismo_fk FOREIGN KEY(id_pisma) REFERENCES Pismo(id_pisma),
-CONSTRAINT sprzedarz_salon_fk FOREIGN KEY(id_salonu) REFERENCES Salon(id_salonu),
-CONSTRAINT sprzedarz_czas_fk FOREIGN KEY(id_czasu) REFERENCES Czas(id_czasu)
+CREATE TABLE Platnosci (
+id_platnosci NUMBER NOT NULL CONSTRAINT platnosci_pk PRIMARY KEY,
+nazwa VARCHAR2(50)
 );
 
+CREATE TABLE Paragon (
+id_paragonu NUMBER NOT NULL CONSTRAINT paragon_pk PRIMARY KEY,
+id_platnosci NUMBER NOT NULL,
+do_zaplaty NUMBER,
 
-DROP TABLE Sprzedaz CASCADE CONSTRAINTS;
-DROP TABLE Pismo CASCADE CONSTRAINTS;
-DROP TABLE Tematyka CASCADE CONSTRAINTS;
-DROP TABLE Dodatek CASCADE CONSTRAINTS;
-DROP TABLE Salon CASCADE CONSTRAINTS;
-DROP TABLE Miasto CASCADE CONSTRAINTS;
-DROP TABLE Czas CASCADE CONSTRAINTS;
-DROP TABLE Sprzedawca CASCADE CONSTRAINTS;
-DROP TABLE Kierownik CASCADE CONSTRAINTS;
-DROP TABLE Osoby CASCADE CONSTRAINTS;
-
-CREATE TABLE Osoby (
-id_osoby NUMBER CONSTRAINT osoby_pk PRIMARY KEY,
-imie VARCHAR2(25) NOT NULL,
-nazwisko VARCHAR(50) NOT NULL,
-data_ur DATE NOT NULL,
-pesel VARCHAR2(11) NULL CONSTRAINT os_pesel_uni UNIQUE,
-adres VARCHAR2(60) NOT NULL,
-telefon VARCHAR2(20) NOT NULL
-);
-
-CREATE TABLE Kierownik (
-id_kierownika NUMBER CONSTRAINT kierownik_pk PRIMARY KEY,
-id_osoby NUMBER NOT NULL,
-data_zatrudnienia DATE NOT NULL,
-data_zwolnienia DATE,
-pensja NUMBER(6) NOT NULL,
-
-CONSTRAINT kier_os_fk FOREIGN KEY(id_osoby) REFERENCES Osoby(id_osoby)
-);
-
-CREATE TABLE Sprzedawca (
-id_sprzedawcy NUMBER CONSTRAINT Sprzedawca PRIMARY KEY,
-id_osoby NUMBER NOT NULL,
-id_kierownika NUMBER NOT NULL,
-data_zatrudnienia DATE NOT NULL,
-data_zwolnienia DATE,
-pensja NUMBER(6) NOT NULL,
-ocena NUMBER(1),                   ---------od 1 do 5
-
-CONSTRAINT sprzed_os_fk FOREIGN KEY(id_osoby) REFERENCES osoby(id_osoby),
-CONSTRAINT sprzed_kier_fk FOREIGN KEY(id_kierownika) REFERENCES Kierownik(id_kierownika)
-);
-
-CREATE TABLE Czas (
-id_czasu NUMBER NOT NULL CONSTRAINT czas_pk PRIMARY KEY,
-data DATE NOT NULL,
-kwartal NUMBER(1) NOT NULL
-);
-
-CREATE TABLE Miasto (
-id_miasta NUMBER NOT NULL CONSTRAINT miasto_pk PRIMARY KEY,
-nazwa VARCHAR2(50) NOT NULL,
-kraj VARCHAR2(40) NOT NULL,
-liczba_mieszkancow NUMBER(7)
-);
-
-CREATE TABLE Salon(
-id_salonu NUMBER NOT NULL CONSTRAINT salon_pk PRIMARY KEY,
-id_miasta NUMBER NOT NULL,
-nazwa VARCHAR2(60) NOT NULL,
-adres VARCHAR2(30) NOT NULL,
-powierzchnia NUMBER(4) NOT NULL,
-obrot_miesieczny NUMBER(7) NOT NULL,
-
-CONSTRAINT salon_miasto_fk FOREIGN KEY(id_miasta) REFERENCES miasto(id_miasta)
-);
-
-
-CREATE TABLE Dodatek (
-id_dodatku NUMBER NOT NULL CONSTRAINT dodatek_pk PRIMARY KEY,
-nazwa VARCHAR2(50) NOT NULL,
-naklad NUMBER(7) NOT NULL
-);
-
-CREATE TABLE Tematyka (
-id_tematyki NUMBER NOT NULL CONSTRAINT tematyka_pk PRIMARY KEY,
-nazwa VARCHAR2(50) NOT NULL,
-opis varchar2(200)
-);
-
-CREATE TABLE Pismo (
-id_pisma NUMBER NOT NULL CONSTRAINT pismo_pk PRIMARY KEY,
-id_dodatku NUMBER,
-id_tematyki number NOT NULL,
-okres_wydawania VARCHAR2(30) NOT NULL,  --np tygodnik itp
-nazwa VARCHAR(40) NOT null,
-naklad NUMBER(7) NOT NULL,
-cena NUMBER(3) NOT NULL,
-
-CONSTRAINT pismo_dodatek_fk FOREIGN KEY(id_dodatku) REFERENCES dodatek(id_dodatku),
-CONSTRAINT pismo_tematyka_fk FOREIGN KEY(id_tematyki) REFERENCES tematyka(id_tematyki)
+CONSTRAINT paragon_PLATNOSCI_fk FOREIGN KEY(id_platnosci) REFERENCES Platnosci(id_platnosci) 
 );
 
 CREATE TABLE Sprzedaz (
@@ -207,23 +100,12 @@ id_pisma NUMBER NOT NULL,
 id_salonu NUMBER NOT NULL,
 id_czasu NUMBER NOT NULL,
 id_sprzedawcy NUMBER NOT NULL,
-ilosc_sprzedanych NUMBER NOT NULL,
+id_paragonu NUMBER NOT NULL
 suma_zysku NUMBER NOT NULL,
 
 CONSTRAINT sprzedarz_pismo_fk FOREIGN KEY(id_pisma) REFERENCES Pismo(id_pisma),
 CONSTRAINT sprzedarz_salon_fk FOREIGN KEY(id_salonu) REFERENCES Salon(id_salonu),
-CONSTRAINT sprzedarz_czas_fk FOREIGN KEY(id_czasu) REFERENCES Czas(id_czasu)
+CONSTRAINT sprzedarz_dzien_fk FOREIGN KEY(id_dnia) REFERENCES Dzien(id_dnia),
+
 );
-
-
-SELECT * FROM osoby;
-SELECT * FROM kierownik;
-SELECT * FROM sprzedawca;
-SELECT * FROM czas;
-SELECT * FROM salon;
-SELECT * FROM miasto;
-SELECT * FROM dodatek;
-SELECT * FROM tematyka;
-SELECT * FROM pismo;
-SELECT * FROM sprzedaz;
 
